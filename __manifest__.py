6b92dc5a26fd513b7f83d8f7b22a5a3f8eb07cc0
Keiko <tatianymori@gmail.com>
2022-10-09 16:11:50 -0300

    Addon Whatsapp init

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Whatsapp Messenger",
+
+    'summary': """
+        """,
+
+    'description': """
+    Send a message with Whatsapp
+    """,
+
+    'author': "Keiko Develop",
+    'website': "",
+    'category': 'Extra Tools',
+    'version': '1.0',
+    'contributors': 'Tatiany Keiko Mori <tatianymori@gmail.com>',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+    ],
+
+    # always loaded
+    'data': [
+    ],
+    'qweb': [
+    ],
+    'css': []
+}
